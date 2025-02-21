75d3eab538c55986c12532fb5e9f8a1ebc36e410
imjustjayk <brycekrause@icloud.com>
2025-02-21 08:47:43 -0600

    improved title page and added image links

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 popularAnimeContainer = document.getElementById("popularAnimeDiv"); // https://api.jikan.moe/v4/top/anime
-recommendedAnimeContainer = document.getElementById("recommendedAnimeDiv"); // https://api.jikan.moe/v4/recommendations/anime
-randomAnimeContainer = document.getElementById("randomAnimeDiv"); // https://api.jikan.moe/v4/random/anime
+recommendedAnimeContainer = document.getElementById("recommendedAnimeDiv"); // https://api.jikan.moe/v4/recommendations/anime 
+// https://api.jikan.moe/v4/random/anime
 
 popularAnimeArr = [];
 recommendedAnimeArr = [];
-randomAnimeArr = [];
 
 popularMangaContainer = document.getElementById("popularMangaDiv"); // https://api.jikan.moe/v4/top/manga
 recommendedMangaContainer = document.getElementById("recommendedMangaDiv"); // https://api.jikan.moe/v4/recommendations/manga
-randomMangaContainer = document.getElementById("randomMangaDiv"); // https://api.jikan.moe/v4/random/manga
+// https://api.jikan.moe/v4/random/manga
 
 popularMangaArr = [];
 recommendedMangaArr = [];
-randomMangaArr = [];
 
 document.addEventListener('DOMContentLoaded', function() {
     // popular anime
@@ -25,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 popularAnimeArr.push(response.data[i]);
             }
             for (var i = 0; i < popularAnimeArr.length; i++) {
-                popularAnimeContainer.innerHTML += "<a href='anime.html?id=" + popularAnimeArr[i].mal_id + "'><img src='" + popularAnimeArr[i].images.jpg.image_url + "'></a>";
+                popularAnimeContainer.innerHTML += "<div class='optionContainer'><a href='selection.html?id=" + popularAnimeArr[i].mal_id + "'><img src='" + popularAnimeArr[i].images.jpg.image_url + "'></a><p>" + popularAnimeArr[i].title + "</p></div>";
             }
         })
         .catch(error => {
@@ -40,14 +38,47 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => {
             for (var i = 0; i < 5; i++) {
                 recommendedAnimeArr.push(response.data[i].entry[0]);
-                console.log(recommendedAnimeArr[i].images.jpg.image_url);
             }
             for (var i = 0; i < recommendedAnimeArr.length; i++) {
-                recommendedAnimeContainer.innerHTML += "<img src='" + recommendedAnimeArr[i].images.jpg.image_url + "'>";
+                recommendedAnimeContainer.innerHTML += "<div class='optionContainer'><a href='selection.html?id=" + recommendedAnimeArr[i].mal_id + "'><img src='" + recommendedAnimeArr[i].images.jpg.image_url + "'></a><p>" + recommendedAnimeArr[i].title + "</p></div>";
             }
         })
         .catch(error => {
             console.log(error);
         });
 
+    // popular manga
+    fetch("https://api.jikan.moe/v4/top/manga")
+        .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            for (var i = 0; i < 5; i++) {
+                popularMangaArr.push(response.data[i]);
+            }
+            for (var i = 0; i < popularMangaArr.length; i++) {
+                popularMangaContainer.innerHTML += "<div class='optionContainer'><a href='selection.html?id=" + popularMangaArr[i].mal_id + "'><img src='" + popularMangaArr[i].images.jpg.image_url + "'></a><p>" + popularMangaArr[i].title + "</p></div>";
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+    // recommended manga
+    fetch("https://api.jikan.moe/v4/recommendations/manga")
+        .then(response => {
+            return response.json()
+        })
+        .then(response => {
+            for (var i = 0; i < 5; i++) {
+                recommendedMangaArr.push(response.data[i].entry[0]);
+                console.log(recommendedMangaArr[i].images.jpg.image_url);
+            }
+            for (var i = 0; i < recommendedAnimeArr.length; i++) {
+                recommendedMangaContainer.innerHTML += "<div class='optionContainer'><a href='selection.html?id=" + recommendedMangaArr[i].mal_id + "'><img src='" + recommendedMangaArr[i].images.jpg.image_url + "'></a><p>" + recommendedMangaArr[i].title + "</p></div>";
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
 });
