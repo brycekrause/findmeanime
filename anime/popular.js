b85fb1c461c2b421d1f2d3b247fdee067e35aabf
imjustjayk <brycekrause@icloud.com>
2025-04-28 10:02:40 -0500

    further fix title selection

--- anime/popular.js
+++ anime/popular.js
@@ -34,7 +34,8 @@ document.addEventListener("DOMContentLoaded", function() {
                     if (!popularAnimeArr.some(item => item.mal_id === response.data[i].mal_id)) {
                         if (!response.data[i].rating.includes("Rx")){
                             popularAnimeArr.push(response.data[i]);
-                            animeSection.innerHTML += "<div class='optionContainer'><a href='selection.html?id=" + response.data[i].mal_id + "'><img src='" + response.data[i].images.jpg.image_url + "'></a><p>" + response.data[i].title_english + "</p></div>";
+                            let title = response.data[i].title_english === null ? response.data[i].title : response.data[i].title_english;
+                            animeSection.innerHTML += "<div class='optionContainer'><a href='selection.html?id=" + response.data[i].mal_id + "'><img src='" + response.data[i].images.jpg.image_url + "'></a><p>" + title + "</p></div>";
                             itemsAdded++;
                         }
 
